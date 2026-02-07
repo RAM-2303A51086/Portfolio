06b67f246da7ffaf3b561fd6ccb2e44447bad511
Private Account <pa2952867@gmail.com>
2026-02-07 14:18:10 +0000

    the left side an dright side header button are very coner if page somw w

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,13 @@
 export function Footer() {
   return (
     <footer className="w-full border-t bg-card">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Designed and Developed by Abhiram Yadav
-          </p>
-        </div>
-        <div>
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Copyright © 2026 AY
-          </p>
-        </div>
+      <div className="container flex flex-col items-center justify-center gap-4 py-6">
+        <p className="text-center text-sm text-muted-foreground">
+          Designed and Developed by Abhiram Yadav
+        </p>
+        <p className="text-center text-sm text-muted-foreground">
+          Copyright © 2026 AY
+        </p>
       </div>
     </footer>
   );
