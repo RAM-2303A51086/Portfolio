2c221ba27a43b16caf2d10ab1e3752b6101e5da3
Private Account <pa2952867@gmail.com>
2026-02-08 07:01:51 +0000

    hi there text should be more right in desktpo view

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -9,7 +9,7 @@ export function HeroSection() {
       id="home"
       className="flex min-h-[calc(100vh-4rem)] items-center justify-start border-b-0 py-0"
     >
-      <div className="flex flex-col items-start justify-center text-left">
+      <div className="flex flex-col items-start justify-center text-left md:pl-20">
         <div className="space-y-2">
           <h1 className="font-headline text-3xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none text-foreground">
             Hi There!{' '}
