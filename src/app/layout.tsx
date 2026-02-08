a4999fd2d826431c76a27c2d2f5045454075bf86
Private Account <pa2952867@gmail.com>
2026-02-08 05:31:38 +0000

    no background applied?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       </head>
       <body
         className={cn(
-          'min-h-screen bg-background font-body antialiased',
+          'min-h-screen font-body antialiased',
           'font-body'
         )}
       >
